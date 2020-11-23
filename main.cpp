32628591c8856e9136cd9223fa42ebbd71745202
Raju Kumar <rakumar@csumb.edu>
2020-11-22 19:11:24 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
+/*
+ * Simple .h file with function prototyping
+ */
+
+
+int main() {
+
+    doSomething();
+
+    return 0;
+}
+void doSomething(){
+    cout << "Hello World!" << endl;
+}
---
+++ utils.h
@@ -0,0 +1,11 @@
+//
+// Created by Raju on 11/22/2020.
+//
+
+#ifndef UNTITLED_UTILS_H
+#define UNTITLED_UTILS_H
+
+void doSomething();
+
+
+#endif //UNTITLED_UTILS_H
